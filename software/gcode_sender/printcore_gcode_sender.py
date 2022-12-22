47637b4e656872f14f43b13d8b5c4410aef2ee3e
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2022-12-22 15:09:09 -0500

    Update printcore_gcode_sender.py

--- software/gcode_sender/printcore_gcode_sender.py
+++ software/gcode_sender/printcore_gcode_sender.py
@@ -15,7 +15,7 @@ while not p.online:
 p.startprint(gcode) # this will start a print
 
 #If you need to interact with the printer:
-p.send_now("M105") # this will send M105 immediately, ahead of the rest of the print
-p.pause() # use these to pause/resume the current print
-p.resume()
-p.disconnect() # this is how you disconnect from the printer once you are done. This will also stop running prints.
+# p.send_now("M105") # this will send M105 immediately, ahead of the rest of the print
+# p.pause() # use these to pause/resume the current print
+# p.resume()
+# p.disconnect() # this is how you disconnect from the printer once you are done. This will also stop running prints.
