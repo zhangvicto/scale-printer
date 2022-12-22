523c11f03a09b4df974cd0f10c28f0020e648d92
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2022-12-22 11:58:08 -0500

    Create test_load_cell.py

---
+++ software/load_cell/test_load_cell.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO  # import GPIO
+
+# init GPIO (should be done outside HX711 module in case you are using other GPIO functionality)
+GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+from hx711 import HX711
+
+try:
+    hx711 = HX711(
+        dout_pin=20,
+        pd_sck_pin=26,
+        channel='A',
+        gain=64
+    )
+
+    hx711.reset()   # Before we start, reset the HX711 (not obligate)
+    measures = hx711.get_raw_data(num_measures=3)
+finally:
+    GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    print("\n".join(measures))
