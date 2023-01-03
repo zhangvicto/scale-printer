d653284987718d89529fefe7338610484b0ecae9
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-03 13:51:12 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -13,7 +13,6 @@ img = cv2.imread('test.jpg')
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-new, edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
 
-if new:
-    cv2.imwrite('edges.jpg', edges, [cv2.IMWRITE_JPEG_QUALITY, 100]) # write to a file
+cv2.imwrite('edges.jpg', edges, [cv2.IMWRITE_JPEG_QUALITY, 100]) # write to a file
