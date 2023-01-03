e7a5ea32af5bc75c38c269df539ae399376fa961
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-03 13:43:33 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -1,8 +1,10 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
+time.sleep(1)
 if ret:
-    cv2.imwrite('test.jpg', frame)
+    cv2.imwrite('test.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
 
 cap.release()
