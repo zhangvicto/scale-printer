44cf89f843a93fab5e4f4e8c4e7fd57b38b5fbb3
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-03 12:42:49 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -1,8 +1,8 @@
-import cv2
-import time
-import picamera2
+# import cv2
+# import time
+from picamera2 import Picamera2
 
-picam2 = picamera2.PiCamera()
-time.sleep(2)
+picam2 = Picamera2()
+
 picam2.start_and_capture_file("test.jpg")
 
