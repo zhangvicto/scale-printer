7645abc42b7aeaa81706267a86b0b72690eac550
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-04 16:20:15 -0500

    test update

--- software/gcode_sender/printcore_gcode_sender.py
+++ software/gcode_sender/printcore_gcode_sender.py
@@ -5,12 +5,12 @@ from printrun import gcoder
 import time
 
 p=printcore('/dev/ttyACM0', 115200) # or p.printcore('COM3',115200) on Windows
-gcode=[i.strip() for i in open('test.gcode')] # or pass in your own array of gcode lines instead of reading from a file
+gcode=[i.strip() for i in open('gcode-test.gcode')] # or pass in your own array of gcode lines instead of reading from a file
 gcode = gcoder.LightGCode(gcode)
 
 # startprint silently exits if not connected yet
-# while not p.online:
-#   time.sleep(0.1)
+while not p.online:
+  time.sleep(0.1)
 
 p.startprint(gcode) # this will start a print
 
