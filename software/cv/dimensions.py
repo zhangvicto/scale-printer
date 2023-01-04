78a4711e5fe320b6b528d390d3c287c8bef86d76
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-04 17:11:53 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -8,22 +8,26 @@ def capture():
     time.sleep(1) # give time to prevent a green image
     if ret:
         cv2.imwrite('test.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100]) # write to a file
-
     cap.release() # release
 
 def image_process(): 
     img = cv2.imread('test.jpg')
     img_rotate = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    print(img_rotate.shape[:2])
+    # print(img_rotate.shape[:2]) # output dimensions
     img_cropped = img_rotate[0:480, 0:480] # crop
+
     img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-
     cv2.imwrite('blur.jpg', img_blur) # write to a file
 
     edges = cv2.Canny(image=img_blur, threshold1=50, threshold2=200) # Canny Edge Detection
-
     cv2.imwrite('edges.jpg', edges) # write to a file
 
 def analyze_edge(): 
     img = cv2.imread('edges.jpg')
+    contours = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.imwrite("output.jpg", contours)
+
+capture()
+image_process()
+analyze_edge
