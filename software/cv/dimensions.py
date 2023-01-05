57f81df4bb49e100304f1863046dd9c403bc2300
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-05 16:37:24 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -8,38 +8,37 @@ cannyThres2 = 180
 
 def capture(): 
     cap = cv2.VideoCapture(0) #setup
-    ret = cap.read() # take image and store in variable
+    ret, frame = cap.read() # take image and store in variable
     time.sleep(2) # give time to prevent a green image
     if ret:
         cap.release() # release
         return ret
-        # cv2.imwrite('test.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100]) # write to a file
+        cv2.imwrite('test.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100]) # write to a file
     else: 
         cap.release() # release
         return 0
     
 
-def image_process(img): 
-    if img: 
-        # img = cv2.imread('test.jpg')
-        img_rotate = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        # print(img_rotate.shape[:2]) # output dimensions
-        img_cropped = img_rotate[0:440, 0:480] # crop
+def image_process(): 
+    img = cv2.imread('test.jpg')
+    img_rotate = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    # print(img_rotate.shape[:2]) # output dimensions
+    img_cropped = img_rotate[0:440, 0:480] # crop
 
-        img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
-        img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-        cv2.imwrite('blur.jpg', img_blur) # write to a file
+    img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+    cv2.imwrite('blur.jpg', img_blur) # write to a file
 
-        edges = cv2.Canny(image=img_blur, threshold1=cannyThres1, threshold2=cannyThres2) # Canny Edge Detection
-        cv2.imwrite('edges.jpg', edges) # write to a file
+    edges = cv2.Canny(image=img_blur, threshold1=cannyThres1, threshold2=cannyThres2) # Canny Edge Detection
+    cv2.imwrite('edges.jpg', edges) # write to a file
 
-        # img = cv2.imread('edges.jpg')
-        contours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(img_cropped, contours[0], -1, color=(255,255,255), thickness=1)
-        time.sleep(1) # give time to prevent a green image
-        cv2.imwrite("output.jpg", img_cropped)
+    # img = cv2.imread('edges.jpg')
+    contours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img_cropped, contours[0], -1, color=(255,255,255), thickness=1)
+    time.sleep(1) # give time to prevent a green image
+    cv2.imwrite("output.jpg", img_cropped)
 
-        return img_blur
+    return img_blur
 
 def analyze_edge(img): 
     # img is blurred
@@ -124,4 +123,5 @@ def hough_coord(lines, i):
 
     return [x0, y0, a, b]
 
-analyze_edge(image_process(capture()))
+capture()
+analyze_edge(image_process())
