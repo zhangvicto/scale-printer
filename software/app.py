ef207fe3f278ba932ee61fc11a11fd2f18cc96db
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-08 16:43:55 -0500

    Update app.py

--- software/app.py
+++ software/app.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "nothing here yet"
-    
-app.run(host='0.0.0.0', port= 8090)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port= 8090)
