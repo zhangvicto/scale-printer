daaac9bf436604745ac7503046d7245699c3a2a1
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-08 17:57:51 -0500

    Update app.py

--- software/app.py
+++ software/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory, request, jsonify
 
-app = Flask(__name__)
-static_path = './web_server/static'
+app = Flask(__name__, static_folder='./web_server/static', static_url_path='/')
+
 
 @app.route("/")
 def index():
-    return send_from_directory('index.html', static_path)
+    return send_from_directory(app.static_folder, 'index.html')
 
 # Use this 
 # flask run --host=0.0.0.0
