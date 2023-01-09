1b5567b40581dfcbc2c4bd4fab19e148813c8ab8
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-08 23:35:59 -0500

    Update

--- software/app.py
+++ software/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory, request, jsonify
+from calibration_setup import calibrate
 
 app = Flask(__name__, static_folder='./web_server/static', static_url_path='/')
 
@@ -6,6 +7,19 @@ app = Flask(__name__, static_folder='./web_server/static', static_url_path='/')
 @app.route("/")
 def index():
     return send_from_directory(app.static_folder, 'index.html')
+
+# Create an API endpoint to talk to the frontend
+@app.route("/api", methods=['GET'])
+def api():
+    # Get the data from the request
+    data = request.get_json()
+    # If data is not empty call calibration function
+    calibrate(data) # pass in data array
+        # Call calibration function
+        # calibrate(data)
+        # Return success
+        return jsonify({'success': True})
+
 
 # Use this 
 # flask run --host=0.0.0.0
--- software/web_server/static/script.js
+++ software/web_server/static/script.js
@@ -1,24 +1,22 @@
-//Simple RESTful API
-import 
+// Create a simple RESTFul API
 
+//Simple RESTful API
 function startPSO() {
     let mode = document.getElementById('mode').value;
+    let data = {
+        mode: mode
+    };
 
-    switch(mode) {
-        case 'line': 
-
-            break;
-
-        case 'plane': 
-
-            break;
-
-        case 'cube': 
-
-            break;
+    fetch('http://localhost:5000/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then((res) => {
+        return res.text();
+    })
+    .then((text) => {
     }
-}
-
-function clear() {
-    
 }
