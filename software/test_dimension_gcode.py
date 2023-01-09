1337f76dc936f75f5990515b0f72cc7288e92a4b
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-09 16:58:57 -0500

    Update test_dimension_gcode.py

--- software/test_dimension_gcode.py
+++ software/test_dimension_gcode.py
@@ -6,15 +6,15 @@ from cv.dimensions import image_process, edges, analyze_edge, find_dim
 iter = 3
 mode = 'L'
 
-# with open("./gcode_gen/test.gcode", "w") as f:
+with open("./gcode_gen/test.gcode", "w") as f:
     
-#     gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
-#     gcode += gcode_gen(mode, iter, settings)
-#     gcode += genEnd(iter)
+    gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
+    gcode += gcode_gen(mode, iter, settings)
+    gcode += genEnd(iter)
 
-#     f.write(gcode)
+    f.write(gcode)
 
-# send_gcode('./gcode_gen/test.gcode')
+send_gcode('./gcode_gen/test.gcode')
 
 # Find Dimension of the Print
 blurred = image_process() # Process Image
