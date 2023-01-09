ef766e00d3b4051cae79c3e5af5776d58450f7c1
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-09 18:29:31 -0500

    Update

--- software/test_dimension_gcode.py
+++ software/test_dimension_gcode.py
@@ -3,42 +3,44 @@ from gcode_gen.generate import gcode_gen, genStart, genEnd, settings
 from gcode_sender.printcore_gcode_sender import send_gcode
 from cv.dimensions import image_process, edges, analyze_edge, find_dim
 
-iter = 3
-mode = 'L'
+for i in range(1, 9):
 
-with open("./gcode_gen/test.gcode", "w") as f:
-    
-    gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
-    gcode += gcode_gen(mode, iter, settings)
-    gcode += genEnd(iter)
+    iter = i
+    mode = 'L'
 
-    f.write(gcode)
+    with open("./gcode_gen/test.gcode", "w") as f:
+        
+        gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
+        gcode += gcode_gen(mode, iter, settings)
+        gcode += genEnd(iter)
 
-send_gcode('./gcode_gen/test.gcode')
+        f.write(gcode)
 
-# Find Dimension of the Print
-blurred = image_process() # Process Image
-edge = edges(blurred) # Canny Edge Detection
+    send_gcode('./gcode_gen/test.gcode')
 
-distX = analyze_edge(edge) # Get the bed x-axis length in terms of pixels
+# # Find Dimension of the Print
+# blurred = image_process() # Process Image
+# edge = edges(blurred) # Canny Edge Detection
 
-# Calculate Print Location
-ratio = round(distX/250) # Pixels per mm
-# Pixel = mm * ratio
+# distX = analyze_edge(edge) # Get the bed x-axis length in terms of pixels
 
-if mode == 'L': 
-    if distX > 0: 
-        x = [round((iter-1)*15*ratio), round(((iter+1)*15+10)*ratio)]
-        print(x)
-        y = [0, 180*ratio]
-    else: 
-        x = [0, 0]
-        y = [0, 0]
-# TBD
-if mode == 'P' or mode == 'C': 
-    x = [0, 0]
-    y = [0, 0]
+# # Calculate Print Location
+# ratio = round(distX/250) # Pixels per mm
+# # Pixel = mm * ratio
 
-dimensions = find_dim(x, y, distX, edge) # Find dim
-widths = dimensions[0]
-lengths = dimensions[1]
+# if mode == 'L': 
+#     if distX > 0: 
+#         x = [round((iter-1)*15*ratio), round(((iter+1)*15+10)*ratio)]
+#         print(x)
+#         y = [0, 180*ratio]
+#     else: 
+#         x = [0, 0]
+#         y = [0, 0]
+# # TBD
+# if mode == 'P' or mode == 'C': 
+#     x = [0, 0]
+#     y = [0, 0]
+
+# dimensions = find_dim(x, y, distX, edge) # Find dim
+# widths = dimensions[0]
+# lengths = dimensions[1]
