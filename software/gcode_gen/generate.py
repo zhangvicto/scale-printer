c352da3bdddf3dcebc9e3a82d5845c58eff87038
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-09 21:24:52 -0500

    Update generate.py

--- software/gcode_gen/generate.py
+++ software/gcode_gen/generate.py
@@ -535,14 +535,14 @@ def createBox(min_x, min_y, size_x, size_y, basicSettings, optional):
     print(gcode)                        
     return gcode
 
-with open("test.gcode", "w") as f:
-    iter = 2
-    configEnd = open("end.txt", "r").read()
-    gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
-    gcode += gcode_gen('P', iter, settings)
-    gcode += genEnd(iter)
-    gcode += configEnd
+# with open("test.gcode", "w") as f:
+#     iter = 2
+#     configEnd = open("end.txt", "r").read()
+#     gcode = genStart(iter=iter, nozzleD=0.4, Te=230, Tb=0, Vp=settings['moveSpeed'])
+#     gcode += gcode_gen('P', iter, settings)
+#     gcode += genEnd(iter)
+#     gcode += configEnd
 
-    f.write(gcode)
+#     f.write(gcode)
 
-    f.close()
+#     f.close()
