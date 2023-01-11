c5f4f3672a2186ebb36d014d45e97de335ca6105
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-01-10 20:21:03 -0500

    Update dimensions.py

--- software/cv/dimensions.py
+++ software/cv/dimensions.py
@@ -33,7 +33,7 @@ def image_process():
     img = cv2.imread('capture0.jpg')
     img_rotate = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     # print(img_rotate.shape[:2]) # output dimensions
-    img_cropped = img_rotate[110:520, 0:475] # crop
+    img_cropped = img_rotate[115:515, 0:475] # crop
 
     img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
